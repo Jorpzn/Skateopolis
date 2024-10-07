5c3f6b2753aecb11a61153f5e9b0c89684d74b18
Jorpzn <116231759+Jorpzn@users.noreply.github.com>
2024-10-06 22:31:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    document.getElementById('hover-message').style.display = 'block';
+}
+
+function hideMessage() {
+    document.getElementById('hover-message').style.display = 'none';
+}
+
+function showMessage2() {
+    document.getElementById('hover-message').style.display = 'block';
+}
+
+function hideMessage2() {
+    document.getElementById('hover-message').style.display = 'none';
+}
